5ff2e177d427c2b56c84eab98c5c38453024a4f4
Mrid22 <mridulaga@outlook.com>
2025-06-07 22:16:30 +0800

    chore: deploy d9d8a4e3ffe54db7215abef3fd4abbab5caf6cd6

--- js/update-history.js
+++ js/update-history.js
@@ -1,4 +1,4 @@
-import { jsxs, jsx } from 'react/jsx-runtime';
+import { jsx, jsxs } from 'react/jsx-runtime';
 import { Clipboard, showHud } from '@project-gauntlet/api/helpers';
 import { useState, useEffect } from 'react';
 import { List, ActionPanel } from '@project-gauntlet/api/components';
@@ -26,18 +26,20 @@ function View() {
     const filteredData = searchText
         ? data.filter((item) => item.toLowerCase().includes(searchText.toLowerCase()))
         : data;
-    return (jsxs(List, { actions: jsx(ActionPanel, { title: "Actions", children: jsx(ActionPanel.Action, { label: "Add to Clipboard", onAction: async () => {
+    let items = filteredData.map((item, index) => (jsx(List.Item, { id: item, title: item }, index)));
+    return (jsxs(List, { actions: jsx(ActionPanel, { title: "Actions", children: jsx(ActionPanel.Action, { label: "Add to Clipboard", onAction: async (id) => {
                     if (searchText) {
                         if (!data.includes(searchText)) {
                             await Clipboard.writeText(searchText);
-                            showHud(searchText + " Added to Clipboard");
-                        }
-                        else {
-                            setSearchText("");
+                            showHud(searchText + " Added to History");
                         }
                     }
-                } }) }), children: [jsx(List.SearchBar, { placeholder: "Search your Clipboard history", value: searchText, onChange: setSearchText }), filteredData.map((item, index) => (jsx(List.Item, { id: `item-${index}`, title: item }, index)))] }));
+                    else if (id) {
+                        await Clipboard.writeText(id);
+                        showHud(searchText + " Copied to Clipboard");
+                    }
+                } }) }), children: [jsx(List.SearchBar, { placeholder: "Search your Clipboard history", value: searchText, onChange: setSearchText }), items] }));
 }
 
 export { View as V, UpdateHistory as default };
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidXBkYXRlLWhpc3RvcnkuanMiLCJzb3VyY2VzIjpbXSwic291cmNlc0NvbnRlbnQiOltdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7OyJ9
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidXBkYXRlLWhpc3RvcnkuanMiLCJzb3VyY2VzIjpbXSwic291cmNlc0NvbnRlbnQiOltdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7In0=
